46544eeeeb33714cd1b123bca8a6332d78c195f7
Jojo <jojobtodd@gmail.com>
2023-01-24 19:43:42 -0600

    Sprite class

--- Stealth/Sprites.cs
+++ Stealth/Sprites.cs
@@ -5,6 +5,35 @@ using System.Text;
 
 namespace Stealth
 {
+    public class Sprite
+    {
+        private byte[,] color;
+        private byte[,] mask;
+
+        private short width;
+        public short Width { get => width; }
+
+        private short height;
+        public short Height { get => height; }
+
+        public Sprite(byte[,] color, byte[,] mask)
+        {
+            if (color.GetLength(0) != mask.GetLength(0) || color.GetLength(1) != 2 * mask.GetLength(1))
+                throw new ArgumentException("Color and mask array size mismatch. Arrays must be mxn and mx2n for mask and color, respectively.");
+
+            this.color = color;
+            this.mask = mask;
+
+            width = (short)(mask.GetLength(1) * 8);
+            height = (short)mask.GetLength(0);
+        }
+
+        public sbyte GetPixel(short x, short y)
+        {
+            // Check if bit at y, x is 1; if so, return 2 bits at y, x
+            return (sbyte)((mask[y, x / 8] & (1 << (x & 7))) == 0 ? -1 : color[y, x / 4] & (3 << (x & 3)));
+        }
+
     public class Sprites
     {
         public sbyte[,] Texture;
