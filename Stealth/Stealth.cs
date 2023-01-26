de94e5c80b1a148a239d86bbf79450f2c8884c72
Jojo <jojobtodd@gmail.com>
2023-01-26 15:46:56 -0600

    Simplify

--- Stealth/Stealth.cs
+++ Stealth/Stealth.cs
@@ -252,7 +252,7 @@ namespace Stealth
 
                     // Render State.Player
                     {
-                        short i = (short)Math.Floor(State.Player.Sprite.Width + 8 * (((((double)CAMERA_WIDTH * x * INVERSE_SCREEN_WIDTH) - (CAMERA_WIDTH * 0.5)) * (CAMERA_DISTANCE + State.Player.Yfine) * INVERSE_CAMERA_DISTANCE) + (CAMERA_WIDTH * 0.5 - State.Player.Xfine + xCam - 1 - CAMERA_WIDTH * 0.5)));
+                        short i = (short)Math.Floor(State.Player.Sprite.Width + 8 * (((((double)CAMERA_WIDTH * x * INVERSE_SCREEN_WIDTH) - (CAMERA_WIDTH * 0.5)) * (CAMERA_DISTANCE + State.Player.Yfine) * INVERSE_CAMERA_DISTANCE) + (xCam - State.Player.Xfine - 1)));
                         short j = (short)(State.Player.Sprite.Height * (((double)y * INVERSE_SCREEN_HEIGHT - 0.5) * (CAMERA_DISTANCE + State.Player.Yfine) * INVERSE_CAMERA_DISTANCE + 0.5));
 
                         if (i >= 0 && i < State.Player.Sprite.Width && j >= 0 && j < State.Player.Sprite.Height && State.Player.Sprite.GetPixel(i, j) >= 0 && ToDepth(State.Player.Yfine) > depth[screenIdx])
